63df717474c01e3639239b4d7b72b49cbc98690b
aryanbethman <30628292+aryanbethman@users.noreply.github.com>
2017-08-05 17:42:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,15 +65,12 @@ function createTemplate (data) {
     var heading = data.heading;
     var content = data.content;
 
-
-
-
-
-
 var htmlTemplate = `
 <html>
    <head>
+   <title>
       ${title}
+      </title>
        
         <meta name="viewport" content="width-device-width, initial-scale=1" />
        
