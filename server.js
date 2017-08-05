da9e47514f94c0a0ace351e9abe5bbc513603b23
aryanbethman <30628292+aryanbethman@users.noreply.github.com>
2017-08-05 16:19:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.get('/article-two.html', function (req, res)
 {res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });  
 
+app.get('/article-three.html', function (req, res)
+{res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});  
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
