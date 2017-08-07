d96c3addbdb659ef9592ec58b8f78ddc10f2c9a4
aryanbethman <30628292+aryanbethman@users.noreply.github.com>
2017-08-07 21:15:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,13 +113,14 @@ app.get('/', function (req, res) {
   //res.send(createTemplate (articles[articleName]));
 //});  
 
-app.get('/ui/style.css', function (req, res) {
+/*app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+*/
 
 app.get('/article-one', function (req, res) {
   res.send(createTemplate(articleOne));
