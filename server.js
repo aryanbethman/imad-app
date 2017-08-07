98ea431f5934c83c0951c2b0105384a81023b096
aryanbethman <30628292+aryanbethman@users.noreply.github.com>
2017-08-07 20:08:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ var articles = {
 
 
 function createTemplate (data) {
-    //var title = data.title;
+    var title = data.title;
     var heading = data.heading;
     var content = data.content;
 
